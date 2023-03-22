2976876fb1e028f12abe73bdb51bc6be66f1f31e
ElgissioFranito <mistera.inwy@gmail.com>
2023-03-22 19:24:01 +0300

    todo list v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isOpen: boolean = false;
+  tache: any[] = [];
+  newTache: string = "";
 
   constructor() {}
+
+  onOpen(){
+    this.isOpen = !this.isOpen;
+    console.log(this.isOpen);
+  }
+
+  onCheck(e: any, i: number){
+    this.tache[i].isCheck = e.detail.checked;
+  }
+
+  onSupprTask(i: number){
+    this.tache.splice(i, 1)
+  }
+
+  onAjouter(){
+    var task = {
+      isCheck : false,
+      nom : this.newTache,
+    }
+  this.tache.push(task);
+  this.newTache= "";
+  this.isOpen = false;
+  }
 
 }
